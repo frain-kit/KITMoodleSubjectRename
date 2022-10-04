e7e72012aed64251c040ec0f90fff3b94b7aa39d
frain-kit <103424766+frain-kit@users.noreply.github.com>
2022-10-04 17:13:35 +0900

    Initial commit

---
+++ content.js
@@ -0,0 +1,65 @@
+if (document.URL.startsWith("https://ict-i.el.kyutech.ac.jp/course/view.php?id=")) {
+    const subjectId = document.URL.substring(document.URL.lastIndexOf("=") + 1);
+    const title = document.title.substring(document.title.indexOf(":") + 2)
+
+    chrome.storage.local.get("subjectNames", function (result) {
+        var map = result.subjectNames
+
+        if (map == undefined || map == null) {
+            map = {}
+        }
+        map[subjectId] = title
+        chrome.storage.local.set({ subjectNames: map }, function () { console.log("Saved " + title + " as " + subjectId) });
+    })
+}
+
+const navs = document.getElementById("nav-drawer").children
+var nav = null
+for (const item of navs) {
+    const attribute = item.getAttribute("aria-label")
+    if (attribute != null && attribute == "サイト") {
+        nav = item
+    }
+}
+
+if (nav == null) {
+    console.log("Could not find the nav.")
+} else {
+    const items = nav.querySelector("ul").children;
+
+    chrome.storage.local.get("subjectNames", function (result) {
+        var nameMap = result.subjectNames
+
+        if (nameMap == undefined || nameMap == null) {
+            nameMap = {}
+        }
+
+        for (const item of items) {
+            var link = item.querySelector("a")
+            if (link == null) {
+                continue;
+            }
+
+            var dataType = link.getAttribute("data-type")
+
+            if (parseInt(dataType, 10) != 20) {
+                continue;
+            }
+
+            const subjectId = parseInt(link.getAttribute("data-key"), 10)
+            try {
+                const subjectName = nameMap[subjectId]
+                if (subjectName == null) {
+                    continue;
+                }
+
+                const mediaBody = link.querySelector(".media-body")
+                if (mediaBody != null) {
+                    mediaBody.textContent = subjectName
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    });
+}
